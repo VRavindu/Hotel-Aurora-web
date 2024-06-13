d82f2431795f73fcf77d2358c824b4a0148046cd
VRavindu <vimukthiravindu01@gmail.com>
2024-06-13 21:15:06 +0530

    Other Styling & Scripts

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const scrollToTopBtn = document.getElementById("scrollToTop");
+
+window.addEventListener("scroll", () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
